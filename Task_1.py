20f4aa40882a4ceaca497917249ec654903fe3f6
shivgarad3162 <garadshiv16@gmail.com>
2025-11-10 22:22:27 +0530

    Add files via upload

---
+++ Task_1.py
@@ -0,0 +1,18 @@
+"""
+programming code for the different mathematical operations
+Addition
+Substraction
+Multiplication
+Division
+by giving input of 2 numbers
+"""
+num1 = float(input("Enter num1 : "))
+num2 = float(input("Enter num2 : "))
+Addition = num1 +num2
+Substraction = num1-num2
+Multiplication = num1*num2
+Division = num1/num2
+print("Addition is :", Addition)
+print("Subtraction is :", Substraction)
+print("Multiplication is :", Multiplication)
+print("Division is :", Division)
---
+++ Task_2.py
@@ -0,0 +1,7 @@
+"""
+Write a Program For greetings
+"""
+First_Name = input("Enter your First_Name :")
+Last_NAme = input("Enter your Last_Name :")
+Full_Name =First_Name +" "+ Last_NAme
+print("Hello,", Full_Name ,"! Welcome to python programming")
