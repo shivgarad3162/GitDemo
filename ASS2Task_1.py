6088454131294ac73367d1b3569c0e9ff9a48c73
shivgarad3162 <garadshiv16@gmail.com>
2026-02-01 11:57:58 +0530

    Add files via upload

---
+++ ASS2Task_1.py
@@ -0,0 +1,12 @@
+## have to check the number is even or odd
+#by using if else statement
+#taking input from the user
+
+
+num1 = float(input("Enter a number:"))
+if num1%2 == 0:
+    print(num1," is an even number.")
+else:
+    print(num1," is an odd number.")
+
+
